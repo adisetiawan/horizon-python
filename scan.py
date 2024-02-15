28bcfe77c5fcec60d89f1e41278358503f22d515
Adi Setiawan <adisetiawan@gmail.com>
2024-02-15 18:51:44 +0800

    added IP validation

--- scan.py
+++ scan.py
@@ -1,4 +1,6 @@
 import socket
+import ipaddress
+import sys
 
 def scan_ports(target, start_port, end_port):
     open_ports = []
@@ -18,16 +20,27 @@ def scan_ports(target, start_port, end_port):
 
     return open_ports
 
+def validate_ip(ip):
+    try:
+        ip_obj = ipaddress.ip_address(ip)
+        return True
+    except ValueError:
+        print(f"ERROR: {ip} is not a valid IP address!")
+        sys.exit()
+        
+
 if __name__ == "__main__":
     target_host = input("Enter the target host IP: ")
     start_port = int(input("Enter the start port: "))
     end_port = int(input("Enter the end port: "))
+    
+    if (validate_ip(target_host)) == True and isinstance(start_port, int) == True and isinstance(end_port, int):
 
-    print(f"Scanning ports {start_port} to {end_port} on {target_host}...")
+        print(f"Scanning ports {start_port} to {end_port} on {target_host}...")
 
-    open_ports = scan_ports(target_host, start_port, end_port)
+        open_ports = scan_ports(target_host, start_port, end_port)
 
-    if open_ports:
-        print("Open ports:", open_ports)
-    else:
-        print("No open ports found.")
+        if open_ports:
+            print("Open ports:", open_ports)
+        else:
+            print("No open ports found.")
